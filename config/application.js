deb29a98f1272d05864ca0cf0c992dbd19f1ea69
Victor Palade <palade.ionut@gmail.com>
2016-08-09 10:06:46 +0200

    fix quotes

--- config/application.js
+++ config/application.js
@@ -1,26 +1,27 @@
-var express = require("express"),
-    consolidate = require("consolidate"),
-    mongoose = require("mongoose"),
-    color = require("colors"),
-    handlebars = require("handlebars"),
-    favicon = require("serve-favicon"),
-    compress = require("compression"),
-    logger = require("morgan"),
-    path = require("path");
+var express = require('express'),
+    consolidate = require('consolidate'),
+    mongoose = require('mongoose'),
+    color = require('colors'),
+    handlebars = require('handlebars'),
+    favicon = require('serve-favicon'),
+    compress = require('compression'),
+    logger = require('morgan'),
+    path = require('path'),
+    env = require('dotenvjs').string();
 
 module.exports  = function(app) {
     //other app settings and vars
     rootPath = path.dirname(path.resolve(__dirname)),
-    viewsPath = path.join(rootPath, "/views"),
-    publicPath = path.join(rootPath, "/public"),
-    faviconPath = path.join(publicPath, "/images/favicon.ico"),
+    viewsPath = path.join(rootPath, '/views'),
+    publicPath = path.join(rootPath, '/public'),
+    faviconPath = path.join(publicPath, '/images/favicon.ico'),
     oneDay = 86400000;
 
     app.use(compress());
     app.use(favicon(faviconPath));
     app.use(express.static(publicPath, {maxAge: oneDay}));
     app.use(logger('dev'));
-    app.engine("html", consolidate.handlebars);
-    app.set("view engine", "html");
-    app.set("views", viewsPath);
+    app.engine('html', consolidate.handlebars);
+    app.set('view engine', 'html');
+    app.set('views', viewsPath);
 }
