81765d9c2acac26c10bef2246d332ded59044a7c
Victor Palade <palade.ionut@gmail.com>
2016-08-09 10:07:33 +0200

    make the server handle different events

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express'),
-    app = module.exports = express(),
-    db = require('database');
+  app = module.exports = express();
+
+// Configure the application, the routes and try to connect to the MongoDB
+
 require('application')(app)
 require('routes')(app)
+require('database');
 
 
-server = app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost' , function() {
+server = app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost')
+
+server.on('error', function(err){
+  console.log('Failed to run instance %s. Terminating...'.red, err)
+  process.exit()
+});
+
+server.once('listening', function() {
     var host = server.address().address;
     var port = server.address().port;
     console.log("Application accessible on http://%s:%s".green, host, port);
