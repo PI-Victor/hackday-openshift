0a85947c80002a8a92e208aa2fff5b865bc964f8
Victor Palade <palade.ionut@gmail.com>
2016-08-15 20:19:37 +0200

    remove extra colons

--- config/routes.js
+++ config/routes.js
@@ -2,13 +2,13 @@ var main = require('main'),
   patients = require('patients'),
   emergency = require('emergency'),
   onduty = require('onduty'),
-  medication = require('medication');
+  medication = require('medication')
 
-module.exports = function(app) {
-  app.get('/', main.index);
-  app.get('/login', main.login);
-  app.get('/patients', patients.add);
-  app.get('/emergency', emergency.show);
-  app.get('/onduty', onduty.show);
-  app.get('/medication', medication.show);
+module.exports = function (app) {
+  app.get('/', main.index)
+  app.get('/login', main.login)
+  app.get('/patients', patients.add)
+  app.get('/emergency', emergency.show)
+  app.get('/onduty', onduty.show)
+  app.get('/medication', medication.show)
 }
