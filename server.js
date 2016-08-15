e6190887ea2fbe37d52b8b859ceabf6120f0ebd7
Victor Palade <palade.ionut@gmail.com>
2016-08-16 01:38:00 +0200

    refactor the way the application works
    * wrap the current app init in the function call to the database

--- server.js
+++ server.js
@@ -1,22 +1,29 @@
 var express = require('express'),
-  app = module.exports = express();
+  database = require('database'),
+  colors = require('colors'),
+  app = module.exports = express()
 
-// Configure the application, the routes and try to connect to the MongoDB
+var hospitality = database()
 
-require('application')(app)
-require('routes')(app)
-require('database');
+/*
+  Bootstrap the application in such a way that it's dependent to the
+  database connection.
+*/
 
+hospitality.connect(function () {
+  // Bootstrap application settings and corresponding routes.
+  require('application')(app)
+  require('routes')(app)
 
-server = app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost')
+  var server = app.listen(process.env.PORT || 3000, process.env.HOST || 'localhost')
 
-server.on('error', function(err){
-  console.log('Failed to run instance %s. Terminating...'.red, err)
-  process.exit()
-});
+  server.on('error', function (err) {
+    console.log('Failed to run instance %s. Terminating...'.red, err)
+  })
 
-server.once('listening', function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("Application accessible on http://%s:%s".green, host, port);
-});
+  server.once('listening', function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log('Application accessible on http://%s:%s'.green, host, port)
+  })
+})
