cd302c48cbbf30acecce12f4c672660048292ed5
Victor Palade <palade.ionut@gmail.com>
2016-08-16 01:38:54 +0200

    refactor application config

--- config/application.js
+++ config/application.js
@@ -1,27 +1,25 @@
 var express = require('express'),
-    consolidate = require('consolidate'),
-    mongoose = require('mongoose'),
-    color = require('colors'),
-    handlebars = require('handlebars'),
-    favicon = require('serve-favicon'),
-    compress = require('compression'),
-    logger = require('morgan'),
-    path = require('path'),
-    env = require('dotenvjs').string();
+  consolidateHbs = require('consolidate').handlebars,
+  favicon = require('serve-favicon'),
+  compress = require('compression'),
+  logger = require('morgan'),
+  path = require('path')
 
-module.exports  = function(app) {
-    //other app settings and vars
-    rootPath = path.dirname(path.resolve(__dirname)),
+
+module.exports = function (app) {
+  // other app settings and vars
+  let rootPath = path.dirname(path.resolve(__dirname)),
     viewsPath = path.join(rootPath, '/views'),
     publicPath = path.join(rootPath, '/public'),
     faviconPath = path.join(publicPath, '/images/favicon.ico'),
-    oneDay = 86400000;
+    oneDay = 86400000
 
-    app.use(compress());
-    app.use(favicon(faviconPath));
-    app.use(express.static(publicPath, {maxAge: oneDay}));
-    app.use(logger('dev'));
-    app.engine('html', consolidate.handlebars);
-    app.set('view engine', 'html');
-    app.set('views', viewsPath);
+  app.use(compress())
+  app.use(favicon(faviconPath))
+  app.use(express.static(publicPath, {maxAge: oneDay}))
+  // remember to change this accordingly.
+  app.use(logger('dev'))
+  app.set('view engine', 'html')
+  app.engine('html', consolidateHbs)
+  app.set('views', viewsPath)
 }
